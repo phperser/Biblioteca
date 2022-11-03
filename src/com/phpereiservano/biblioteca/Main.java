443ed5a45668fb8d064d0beeef0be5f021e93761
TheJagermeister <pedroservano007@gmail.com>
2022-11-03 18:23:33 +0100

    Primeros pasos

--- src/com/phpereiservano/biblioteca/Main.java
+++ src/com/phpereiservano/biblioteca/Main.java
@@ -3,9 +3,5 @@ package com.phpereiservano.biblioteca;
 import com.phpereiservano.biblioteca.Genero;
 
 public class Main {
-    public static void main(String[] args){
-        Genero genero = new genero();
-        System.out.println("Introduzca el nombre del genero");
-        genero.setNombre(scanner.next());
-    }
+
 }
